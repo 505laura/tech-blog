2d7ba20a2c70ebf43b5af2ad6f84e658a6dbe460
laura <laurabranc94@gmail.com>
2022-12-01 17:09:39 +0000

    Add post model

---
+++ models/Post.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        //TODO: add user id and blog id for colabs
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        tags: {
+            type: DataTypes.ENUM(),
+        },
+        views: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
